32de4e735261a232cfc2e317168a7a36d10143a7
MoazElgabry <113203640+MoazElgabry@users.noreply.github.com>
2026-03-27 05:56:08 +0200

    1.0.5Beta

--- source/tools/cube_viewer_stub/main.cpp
+++ source/tools/cube_viewer_stub/main.cpp
@@ -297,7 +297,7 @@ void notifyExistingViewerBringToFront() {
 }
 #endif
 
-const char* kViewerVersionString = "v1.0.4Beta";
+const char* kViewerVersionString = "v1.0.5Beta";
 
 #if !defined(_WIN32)
 bool sendAllSocket(int fd, const char* data, size_t size) {
